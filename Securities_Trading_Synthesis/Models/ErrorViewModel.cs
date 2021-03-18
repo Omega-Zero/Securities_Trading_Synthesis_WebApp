74065b06ff98c9a1ffd951004e13d85db631a6a7
Garrett Kelly <gckelly761@gmail.com>
2021-03-18 13:13:10 -0400

    code commit

---
+++ Securities_Trading_Synthesis/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Securities_Trading_Synthesis.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
